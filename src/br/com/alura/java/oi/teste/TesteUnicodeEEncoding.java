a5f3d72849fa128bb50b8b44218fd24eed3ccbca
ruthe-batista <ruthebatista04@gmail.com>
2022-01-04 16:06:48 -0300

    Encoding no windows

---
+++ src/br/com/alura/java/oi/teste/TesteUnicodeEEncoding.java
@@ -0,0 +1,33 @@
+package br.com.alura.java.oi.teste;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class TesteUnicodeEEncoding {
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+
+        String s = "C";
+        System.out.println(s.codePointAt(0));
+
+
+        Charset charset = Charset.defaultCharset();
+        System.out.println(charset.displayName());
+
+        byte[] bytes = s.getBytes("UTF-8");
+        System.out.print(bytes.length + ", UTF-8,   ");
+        String sNovo = new String(bytes, "UTF-8");
+        System.out.println(sNovo);
+
+        bytes = s.getBytes("windows-1252");
+        System.out.print(bytes.length + ", windows-1252,  ");
+        sNovo = new String(bytes, "windows-1252");
+        System.out.println(sNovo);
+
+        bytes = s.getBytes(StandardCharsets.US_ASCII);
+        System.out.print(bytes.length + ", US-ASCII,   ");
+        sNovo = new String(bytes, "US-ASCII");
+        System.out.println(sNovo);
+    }
+}
