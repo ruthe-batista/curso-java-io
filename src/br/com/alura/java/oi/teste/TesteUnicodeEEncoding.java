61173970d44cfc4bd561cc0fa9f21fb00a0ed25d
ruthe-batista <ruthebatista04@gmail.com>
2022-01-04 19:23:13 -0300

    Serializacao java

--- src/br/com/alura/java/oi/teste/TesteUnicodeEEncoding.java
+++ src/br/com/alura/java/oi/teste/TesteUnicodeEEncoding.java
@@ -9,7 +9,7 @@ public class TesteUnicodeEEncoding {
     public static void main(String[] args) throws UnsupportedEncodingException {
 
         String s = "C";
-        System.out.println(s.codePointAt(0));
+        System.out.println(s.codePointAt(0)); //unicode
 
 
         Charset charset = Charset.defaultCharset();
